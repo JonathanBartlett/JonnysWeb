d6a553dc41f597a518b7a7bc3bb31a27c6329cee
JonathanBartlett <jonathan@jonnysweb.co.uk>
2015-03-30 14:34:35 +0100

    Add Normalize and Index View

--- Gemfile
+++ Gemfile
@@ -43,3 +43,5 @@ group :development, :test do
   gem 'spring'
 end
 
+gem 'haml', '~> 4.0.6'
+
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
 	def index
 	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
 end
